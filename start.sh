e22624460370b33a353159476a5b523842e1833b
ole <ole@ole.local>
2017-12-03 11:37:33 +0800

    update

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 export DB_URL="cailianpress_dba:xxxxxx@tcp(47.93.71.5:3306)/cls_1508?charset=utf8mb4&parseTime=True&loc=Asia%2FShanghai"
 export WRITER_DB_URL="cailianpress_dba:xxxxxx@tcp(47.93.71.5:3306)/cls_1508?charset=utf8mb4&parseTime=True&loc=Asia%2FShanghai"
 export APP_PORT="1323"
-export REDIS_ADDR="101.201.34.157:6333"
+export REDIS_ADDR=""
 export REDIS_PWD=""
 go run main.go
